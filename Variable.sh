e176702ef40911e513843eca28b7cf7fdb2bcde6
Saideepika <912183@cognizant.com>
2023-10-02 12:25:23 +0530

    Added functions

--- Variable.sh
+++ Variable.sh
@@ -13,3 +13,17 @@ echo Arth=${ARTH}
 
 ##If we want to use other files as a source, mention source common.sh(file name)
 ##source cmd -used to import the file
+
+#Properties of variable
+#Read write
+b=10
+b=20
+echo b -$b
+
+#Mark varaibale as readonly for that read only command will help
+readonly b
+b=30
+
+#Local variable -Variables can be access within script
+#If we want to access local varaibles need to use "export" command 
+
